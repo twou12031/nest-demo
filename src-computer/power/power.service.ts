7674fcd638a456cc70ed2aa02155a8a9f9bdf5ba
yefu.zhang <yefu.zhang@yaduo.com>
2021-10-21 01:01:53 +0800

    验证复用class, 不会重复生成实例;

--- src-computer/power/power.service.ts
+++ src-computer/power/power.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class PowerService {
+    constructor() {
+        console.log('@PowerService constructor')
+    }
+
     supplyPower(watts: number) {
         console.log(`@PowerService, watts:${watts}`)
     }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core'
+import { ComputerModule } from '../src-computer/computer/computer.module'
 
 async function bootstrap() {
-    const app = await NestFactory.create()
+    const app = await NestFactory.create(ComputerModule)
 
     await app.listen(3001)
 }
